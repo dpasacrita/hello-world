824e9a596270013cd5941453265ca4f7520c3a87
Dan <Whitewind617@gmail.com>
2016-02-21 16:46:04 -0500

    More code!!!

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
 print("Hello World!!!!!")
+movies = ["The Holy Grail",
+          "The life of brian",
+          "The meaning of life",
+          1]
+
+print(movies)
+print(movies[0])
+print(movies[3])
